374a9e34d99e93767c2ef89aa71c04cd4e4c2340
user <user@DESKTOP-5L8E5H1>
2019-06-20 10:56:40 +0530

    code send

---
+++ src/main/java/com/app/SpringBootDataJpaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaStreamApplication.class, args);
+	}
+
+}
